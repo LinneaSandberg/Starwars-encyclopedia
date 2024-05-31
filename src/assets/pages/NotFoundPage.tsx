ae1a8068c6004bc14be68ef8b862699f5b187961
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-05-31 12:53:05 +0200

    changed css so the notfoundpage can be viewed

--- src/assets/pages/NotFoundPage.tsx
+++ src/assets/pages/NotFoundPage.tsx
@@ -1,11 +1,11 @@
 const NotFoundPage = () => {
     return (
-        <div className="wrapper-giphy">
-            <div className="text-container">
-                <h3>This page does not exist.</h3>
-            </div>
+
+        <div className="text-container">
+            <h3 className="custom-h3">This page does not exist.</h3>
             <img src="https://media.giphy.com/media/YyKPbc5OOTSQE/giphy.gif" className="img-custom" alt="Giphy GIF" />
         </div>
+
     );
 }
 
